89207b751a56167397cf7057b2cd20ba32d6ba4d
Kazakdon <strannik1974@mail.ru>
2024-12-13 20:26:47 +0300

    Типизация массива

---
+++ netkliks-app/src/app/constants/film.constants.ts
@@ -0,0 +1,31 @@
+import { Film } from '../models/film.model';
+
+export const FILMS: Film[] = [
+  {
+    id: 0,
+    title: 'Марсианин',
+    original: 'The Martian 2015',
+    category: 'Фантастика, приключения',
+    rating: 7.8,
+    image: './img/film.png',
+    link: '/film.html',
+  },
+  {
+    id: 1,
+    title: 'Марсианин 1',
+    original: 'The Martian 2015',
+    category: 'Фантастика, приключения',
+    rating: 7.8,
+    image: './img/film.png',
+    link: '/film.html',
+  },
+  {
+    id: 2,
+    link: '/film.html',
+    title: 'Марсианин 2',
+    original: 'The Martian 2015',
+    category: 'Фантастика, приключения',
+    rating: 7.8,
+    image: './img/film.png',
+  },
+];
---
+++ netkliks-app/src/app/models/film.model.ts
@@ -0,0 +1,9 @@
+export interface Film {
+  id: number;
+  title: string;
+  original: string;
+  category: string;
+  rating: 7.8;
+  image: string;
+  link: string;
+}
